158829ac5ee7fb4b2b093e48923c68259ae58814
Purna Chandra Dash <purna2004@gmail.com>
2026-07-01 18:48:27 +0530

    Enhanced logging

---
+++ logger.py
@@ -0,0 +1,21 @@
+import os
+import logging
+import logging.config
+import yaml
+
+def setup_logging():
+    config_path = "logging.yaml"
+
+    if os.path.exists(config_path):
+        with open(config_path, "rt") as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(
+            filename='app.log',
+            filemode='a',
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            level=logging.INFO
+        )
+        logging.warning("logging.yaml not found. Using basic configuration.")
--- main.py
+++ main.py
@@ -1,24 +1,5 @@
+from logger import setup_logging
 import logging
-import logging.config
-import os
-import yaml
-
-def setup_logging():
-    config_path = "logging.yaml"
-
-    if os.path.exists(config_path):
-        with open(config_path, "rt") as f:
-            config = yaml.safe_load(f.read())
-        logging.config.dictConfig(config)
-    else:
-        logging.basicConfig(
-            filename='app.log',
-            filemode='a',
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S',
-            level=logging.INFO
-        )
-        logging.warning("logging.yaml not found. Using basic configuration.")
 
 def main():
     setup_logging()
